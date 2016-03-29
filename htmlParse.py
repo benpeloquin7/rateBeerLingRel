93d02f7747251feedc3cf37eaf909ca9b4472397
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-28 21:35:56 -0700

    locating divs with background color

--- htmlParse.py
+++ htmlParse.py
@@ -4,11 +4,30 @@ import urllib
 import re
 
 html1 = "http://www.ratebeer.com/beer/de-molen--hair-of-the-dog-fred/369740/1786/"
-
-r = urllib.urlopen(html1).read()
-soup = BeautifulSoup(r)
-letters = soup.find_all("div", class_ = "curvy")
+html2 = "http://www.ratebeer.com/beer/ridge-ay-caramba-jalapeno-ipa/405866/1786/"
+html3 = "http://www.ratebeer.com/beer/skookum-murder-of-crows/176960/1786/"
+r = urllib.urlopen(html3).read()
+soup = BeautifulSoup(r, "html.parser")
+#letters = soup.find_all("div", class_ = "curvy")
 #print letters[0]
 #print soup.prettify()
-print soup.get_text()
+#letters2 = soup.find("div", class_ = "curvy")
 
+#print letters[0].get_text()
+
+
+## children of curvy div
+curvy = soup.find_all("div", class_ = "curvy")
+#curvy = soup.find(string=re.compile("background: #ffffdd;"))
+print type(curvy)
+
+for r in curvy:
+    print "==========="
+    currAttributes = r.attrs['style']
+    print re.search("background: #ffffdd;", currAttributes) != None
+
+#print re.findall("#ffffdd;", r)
+#children = curvy.findChildren()
+#for child in children:
+#    print "================="
+#    print child
