4f86ec2391fc5130a46cc82dc6214132411dd546
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-30 21:01:55 -0700

    basic scraping functionality with main() introduced so we can call script directly with url args

--- htmlParse.py
+++ htmlParse.py
@@ -2,23 +2,19 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 import sys
-import pdb
 
-## example urls
-html1 = "http://www.ratebeer.com/beer/de-molen--hair-of-the-dog-fred/369740/1786/"
-html2 = "http://www.ratebeer.com/beer/ridge-ay-caramba-jalapeno-ipa/405866/1786/"
-html3 = "http://www.ratebeer.com/beer/skookum-murder-of-crows/176960/1786/"
 
-## set-up
 def getTitle(soup):
     """
-    return :: beer name
+    Extract beer title
+    return :: string beer name
     """
     return soup.find("h1").get_text()
 
 def getScores(soup):
     """
-    RateBeer.com places target user reviews in class = 'curvy'
+    Get individual attribute scores
+    (RateBeer.com places target user reviews in class = 'curvy')
     returns :: string of individual scores or empty string
     """
     userPattern = "background: #ffffdd;" # identify our user
@@ -30,6 +26,9 @@ def getScores(soup):
     return ""
 
 def targetDiv(div, pattern, attr):
+    """
+    return :: boolean check if we have target div
+    """
     currAttr = div.attrs
     return currAttr and\
         attr in currAttr.keys() and\
@@ -37,6 +36,7 @@ def targetDiv(div, pattern, attr):
 
 def getReviewText(soup):
     """
+    Extract review text
     return :: string review text for target user review
     """
     paddingPattern = "padding: 20px 10px 0px 0px; line-height: 1.5;" # from manual inspection
@@ -47,48 +47,26 @@ def getReviewText(soup):
 
     return ""
 
+def getBeerInfo(soup):
+    """
+    return :: title, scores, and review text
+    """
+    return [getTitle(soup), getScores(soup), getReviewText(soup)]
 
-#    paddingPattern = "padding: 20px 10px 0px 0px; line-height: 1.5;" # from manual inspection
-#    allDivs = soup.find_all("div")
-#                
-#    reviewText = ""
-#    for div in allDivs:
-#        currAttr = div.attrs
-#        if "style" in currAttr.keys() and\
-#                re.search(paddingPattern, currAttr['style']) != None:
-#            reviewText = div.get_text()
+def main():
+    ## example URLs for rater id 1786
+    html1 = "http://www.ratebeer.com/beer/de-molen--hair-of-the-dog-fred/369740/1786/"
+    html2 = "http://www.ratebeer.com/beer/ridge-ay-caramba-jalapeno-ipa/405866/1786/"
+    html3 = "http://www.ratebeer.com/beer/skookum-murder-of-crows/176960/1786/"
+    urls = sys.argv[1:] if len(sys.argv) > 1 else [html1]
 
+    for url in urls:
+        print "---------------------"
+        print "====================="
+        print "Currently scraping title, scores and review text for: \n" + url + "\n"
+        r = urllib.urlopen(url).read()
+        soup = BeautifulSoup(r, "html.parser")
+        print getBeerInfo(soup)
 
-r = urllib.urlopen(html1).read()
-soup = BeautifulSoup(r, "html.parser")
-print getTitle(soup)
-print getScores(soup)
-print getReviewText(soup)
-
-
-
-####
-#### 1) extract name of review
-####
-#h1Text = soup.find("h1").get_text()
-
-
-####
-#### 2) extract individual scores
-####
-#userPattern = "background: #ffffdd;" # identify our user
-#scores = ''
-#for r in curvy:
-#    if "style" in r.attrs and\
-#            re.search(userPattern, r.attrs["style"]):
-#        scores = r.get_text()
-
-####
-#### 3) extract review text
-####
-
-## current data
-#data = (h1Text, scores, reviewText) 
-#print data
-
-
+if __name__ == "__main__":
+    main()
