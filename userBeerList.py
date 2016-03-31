60d686ab045df57560ae51f682b0dd11effa4aa5
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-31 15:05:53 -0700

    initial working version - scrapes all beer review urls so we can pass to beerData.py

--- userBeerList.py
+++ userBeerList.py
@@ -1,20 +1,72 @@
+#########
+######### Given a user beer listing page on RateBeer.com
+######### Return a list of all beer review urls on that page
+######### (Currently using selenium which forces Firefox browser pop-up)
+#########
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import urllib
 import re
 import sys
+import pdb
 
 
-def userBeerList(id):
-    userBeerListPage = "http://www.ratebeer.com/user/" + userId + "/"
-    r = urllib.urlopen(userBeerListPage).read()
-    return BeautifulSoup(r, "html.parser")
+def constructBeerListURL(userID):
+    """
+    Given user ID get list of 50 beers
+    return :: url string
+    """
+    return  "http://www.ratebeer.com/user/" + userID + "/beer-ratings/"
 
-def getBeerUrls(soup):
-    table = soup.find("table", {"class" : "table"})
+def constructTargetBeerURL(partialURL):
+    """
+    Given a partial URL return full url pointing at beer page 
+    (with target user review first)
+    return :: url string
+    """
+    return "http://www.ratebeer.com/" + partialURL
+    
+def convertURLs(urls, fn):
+    """
+    Convert a list of URLs given editing function `fn`
+    return :: list of edited url strings
+    """
+    return [fn(url) for url in urls]
+
+def getUserBeerURLs(beerListURL, userID):
+    """
+    Given user beer list url and id return
+    target urls on this page
+    return :: list of beer url strings
+    """
+    browser = webdriver.Firefox()
+    browser.get(beerListURL)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    allURLs = soup.find_all("a", href = True)
+    pattern = "(beer/.+" + userID + "/)"
+    targetURLs = getTargetURLs(allURLs,  pattern)
+    return targetURLs
+    
+def getTargetURLs(urls, pattern):
+    """
+    Given list of urls, add regex pattern get target urls
+    return : list of target url strings
+    """
+    goodURLs = []
+    for url in urls:
+        match = re.search(pattern, url['href'])
+        if match:
+            goodURLs.append(match.group(0))
+    return goodURLs
 
 
 def main():
-    userId = sys.argv[1]
+    userID = sys.argv[1] if len(sys.argv) > 1 else "1786"
+    beerListPage = constructBeerListURL(userID)
+    print beerListPage
+    beerURLs = getUserBeerURLs(beerListPage, userID)
+    convertedURLs = convertURLs(beerURLs, constructTargetBeerURL)
+    print convertedURLs
 
 if __name__ == "__main__":
     main()
