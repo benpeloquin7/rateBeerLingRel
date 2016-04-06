4c0938e046af16cdcda1ec3122e03fc6f33b8fec
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-05 18:57:51 -0700

    changed top users url

--- RateBeerHelpers.py
+++ RateBeerHelpers.py
@@ -4,6 +4,6 @@
 #########
 
 ## Top raters
-TOP_RATER_BASE_URL = "http://www.ratebeer.com/Users/BeerClub.asp?PageNo=1"
+TOP_RATERS_URL = "http://www.ratebeer.com/Users/TopUsers.asp"
 ## Top 50 beers
 TOP_50_BEERS_URLS = "http://www.ratebeer.com/top"
