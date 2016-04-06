961e0c984dc7f0c326014517499973b639001b73
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-05 18:25:24 -0700

    generic RateBeer useful data -- such as url to top raters and top beers

---
+++ RateBeerHelpers.py
@@ -0,0 +1,9 @@
+#########
+######### RateBeerHelpers.py
+######### RateBeer.com specific data
+#########
+
+## Top raters
+TOP_RATER_BASE_URL = "http://www.ratebeer.com/Users/BeerClub.asp?PageNo=1"
+## Top 50 beers
+TOP_50_BEERS_URLS = "http://www.ratebeer.com/top"
