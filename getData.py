0c07a672c068d11c44e2343151d3b232ee4792a5
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-06 14:00:10 -0700

    practice creating single reviewData object

--- getData.py
+++ getData.py
@@ -8,33 +8,51 @@ import reviewData
 import csv
 
 
-users = scraper.getUserIds(RBhelpers.TOP_RATERS_URL)
+user = "1786"
+url = "http://www.ratebeer.com/beer/parallel-49--cannery-gimme-shelter/409009/1786/"
+soup = scraper.urlToSoup(url)
+data =  soupParser.getBeerInfo(soup)
+id = user
+beerName = soupParser.removeNonAscii(data[0])
+ratingsBlob = soupParser.removeNonAscii(data[1])
+reviewBlob = soupParser.removeNonAscii(data[2])
+data =\
+    reviewData.ReviewData(userID = id,
+    beerName = beerName,
+    ratings = ratingsBlob,
+    review = reviewBlob)
 
-dataHold = []
+data.prettyPrint()
+data.setData()
+data.prettyPrint()
 
-for user in users[0:2]:
-    beerListURL = scraper.constructBeerListURL(user)
-    beerURLs = scraper.getUserBeerURLs(beerListURL, user)
-    for url in beerURLs:
-        soup = scraper.urlToSoup(url)
-        data =  soupParser.getBeerInfo(soup)
-        id = user
-        beerName = soupParser.removeNonAscii(data[0])
-        ratingsBlob = soupParser.removeNonAscii(data[1])
-        reviewBlob = soupParser.removeNonAscii(data[2])
-        data =\
-            reviewData.ReviewData(userID = id,
-            beerName = beerName,
-            ratings = ratingsBlob,
-            review = reviewBlob)
-        # dataDict = {"user":user,\
-        #         "beerName":removeNonAscii(data[0]),\
-        #         "ratings":removeNonAscii(data[1]),\
-        #         "review":removeNonAscii(data[2])}
-        dataHold.append(data)
+# users = scraper.getUserIds(RBhelpers.TOP_RATERS_URL)
+
+# dataHold = []
+
+# for user in users[0:2]:
+#     beerListURL = scraper.constructBeerListURL(user)
+#     beerURLs = scraper.getUserBeerURLs(beerListURL, user)
+#     for url in beerURLs:
+#         soup = scraper.urlToSoup(url)
+#         data =  soupParser.getBeerInfo(soup)
+#         id = user
+#         beerName = soupParser.removeNonAscii(data[0])
+#         ratingsBlob = soupParser.removeNonAscii(data[1])
+#         reviewBlob = soupParser.removeNonAscii(data[2])
+#         data =\
+#             reviewData.ReviewData(userID = id,
+#             beerName = beerName,
+#             ratings = ratingsBlob,
+#             review = reviewBlob)
+#         # dataDict = {"user":user,\
+#         #         "beerName":removeNonAscii(data[0]),\
+#         #         "ratings":removeNonAscii(data[1]),\
+#         #         "review":removeNonAscii(data[2])}
+#         dataHold.append(data)
         
-for i in range(len(dataHold)):
-    print dataHold[i].prettyPrint()
+# for i in range(len(dataHold)):
+#     print dataHold[i].prettyPrint()
 
 # for i in range(len(dataHold)):
 #     print type(i)
