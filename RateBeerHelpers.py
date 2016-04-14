a9b3220bacb6ca7d41fbaa99b1c006dba0fd9e12
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-14 15:35:46 -0700

    user ID bounds

--- RateBeerHelpers.py
+++ RateBeerHelpers.py
@@ -7,3 +7,7 @@
 TOP_RATERS_URL = "http://www.ratebeer.com/Users/TopUsers.asp"
 ## Top 50 beers
 TOP_50_BEERS_URLS = "http://www.ratebeer.com/top"
+
+## Approximate bounds (from manual inspection) of user IDs
+LOWEST_USER_ID = 500
+HIGHEST_USER_ID = 400000
