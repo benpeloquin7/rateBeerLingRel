0d6bb4b4e8d62082c2d1cc85788dc0587bf02166
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-22 11:45:06 -0700

    primary sampleID scraping

--- sampleIDs.py
+++ sampleIDs.py
@@ -8,13 +8,24 @@ import random
 import csv
 from collections import defaultdict
 
+########
+#### NOTE: got rate limited at pause time 30 ,rate_limit_value 20
+####
+
+
+## Seed from 
+
 ## Run data
-random.seed(2)
-numIDs = 100
-RATE_LIMIT_VALUE = 30
-RATE_LIMIT_PAUSE_TIME = 1
+# set seed 7 at 4/19/2016 start time 9:52pm
+####
+
+random.seed(8)
+NUM_IDS = 20000
+RATE_LIMIT_VALUE = 20
+RATE_LIMIT_PAUSE_TIME = 40
 goodIDs = []
 badIDs = []
+path = 'data/'
 
 ## Store IDs that have been seen
 seenIDs = set()
@@ -28,11 +39,9 @@ with open('data/bad.csv') as infile:
 	reader = csv.reader(infile)
 	for rows in reader:
 		seenIDs.add(rows[0])
-print len(seenIDs)
-print seenIDs
 
 # Convert IDs to strings
-userIDs = [str(id) for id in random.sample(range(LOWEST_USER_ID, HIGHEST_USER_ID), numIDs)]
+userIDs = [str(id) for id in random.sample(range(LOWEST_USER_ID, HIGHEST_USER_ID), NUM_IDS)]
 for i, userID in enumerate(userIDs):
 	## Skip seen IDs
 	print i
@@ -45,15 +54,28 @@ for i, userID in enumerate(userIDs):
 	uPage.setBeerList()
 	beerURLs = uPage.getBeerList()
 	
-	## only take reviewers with 3 or more
+	## only take reviewers with 1 or more
 	if len(beerURLs) > 0: goodIDs.append((userID, len(beerURLs)))
 	else: badIDs.append(userID)
 
 	if i % RATE_LIMIT_VALUE == 0:
+		## Interim data logging
+		## Save good Ids and number
+		with open(path + 'good.csv','a') as out:
+		    csv_out = csv.writer(out)
+		    for row in goodIDs:
+		        csv_out.writerow(row)
+
+		## Save good Ids and number
+		with open(path + 'bad.csv', 'a') as out:
+			csv_out = csv.writer(out)
+			for row in badIDs:
+				csv_out.writerow([row])
+		goodIDs = []
+		badIDs = []
 		time.sleep(RATE_LIMIT_PAUSE_TIME)
 
 # Data logging
-path = 'data/'
 if not os.path.exists(path):
 	os.makedirs(path)
 
