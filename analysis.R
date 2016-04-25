72be5b90f7f534a7499d23ba5656c6cae49b0773
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-24 19:01:43 -0700

    added a little code for summaries on runSampling.py

--- analysis.R
+++ analysis.R
@@ -5,16 +5,20 @@ library(tidyr)
 library(dplyr)
 setwd("/Users/benpeloquin/Desktop/Spring2016/CS224U/rateBeerLingRel")
 
+####
 #### Number of scrape
+####
 timeNeededInSeconds <- function(nIDs, pauseTime, pauseValue) {
   nBreaks <- nIDs / pauseValue
   breakSeconds <- nBreaks * pauseTime
   nonBreakSeconds <- (nIDs - nBreaks) * 5
   breakSeconds + nonBreakSeconds
 }
-timeNeededInSeconds(5000, 50, 15) / 60 / 60 ## These are good settings
+timeNeededInSeconds(5000, 40, 20) / 60 / 60 ## These are good settings
 
-## Data
+####
+#### Sample Ids
+####
 d <- read.csv("data/good.csv")
 names(d) <- c("userID", "n")
 numReviews <- sum(d$n)
@@ -37,3 +41,27 @@ printSummary <- function() {
 printSummary()
 
 
+####
+#### run sampling
+####
+print_review_summary <- function() {
+  path <- "data/reviews_store/"
+  files <- list.files(path)
+  d.raw <- data.frame()
+  for (f in files){
+    d <- read.csv(paste0(path, f))
+    d.raw <- rbind(d.raw, d)
+  }
+  n_users <- length(unique(d.raw$user_id))
+  cat(paste0("Number of unique users: ", n_users), "\n")
+  n_reviews <- nrow(d.raw)
+  cat(paste0("Number of reviews: ", n_reviews))
+  
+  d.raw[match(unique(d.raw$user_id), d.raw$user_id),] %>%
+    select(user_id, user_num_ratings) %>%
+    group_by(user_num_ratings) %>%
+    summarise(count = n()) %>%
+    ggplot(aes(x = log(user_num_ratings), y = count)) +
+    geom_bar(stat = "identity")
+}
+print_review_summary()
