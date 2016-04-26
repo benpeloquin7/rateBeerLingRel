fa284bbd1ca259c86e5ac422757ac24259665a8b
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-04-26 13:06:14 -0700

    summaries for missing data, code for outputing ids for missing data

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(stringr)
 setwd("/Users/benpeloquin/Desktop/Spring2016/CS224U/rateBeerLingRel")
 
 ####
@@ -92,3 +93,102 @@ d.raw %>%
          review_appearance_score) %>%
   as.matrix() %>%
   cor(use = "pairwise.complete.obs")
+
+
+missing_data_summary <- function(d.raw) {
+  cat(paste0("-------------- REVIEW ----------------"), "\n")
+  cat(paste0("===================================="), "\n")
+  
+  miss_rev_text <- nrow(d.raw[which(is.na(d.raw$review_blob)), ])
+  cat(paste0("Missing review text: ", miss_rev_text), "\n")
+  
+  miss_rev_overall_score <- nrow(d.raw[which(is.na(d.raw$review_overall_score)), ])
+  cat(paste0("Missing review overall score: ", miss_rev_overall_score), "\n")
+  
+  miss_rev_avg_score <- nrow(d.raw[which(is.na(d.raw$review_blob)), ])
+  cat(paste0("Missing review avg score: ", miss_avg_score), "\n")
+  
+  miss_rev_taste_score <- nrow(d.raw[which(is.na(d.raw$review_taste_score)), ])
+  cat(paste0("Missing review taste score: ", miss_rev_taste_score), "\n")
+  
+  miss_rev_aroma_score <- nrow(d.raw[which(is.na(d.raw$review_aroma_score)), ])
+  cat(paste0("Missing review aroma score: ", miss_rev_aroma_score), "\n")
+  
+  miss_rev_palate_score <- nrow(d.raw[which(is.na(d.raw$review_palate_score)), ])
+  cat(paste0("Missing review palate score: ", miss_rev_palate_score), "\n")
+  
+  miss_rev_appearance_score <- nrow(d.raw[which(is.na(d.raw$review_appearance_score)), ])
+  cat(paste0("Missing review appearance score: ", miss_rev_appearance_score), "\n")
+  
+  
+  cat(paste0("-------------- BEER ----------------"), "\n")
+  cat(paste0("===================================="), "\n")
+  
+  miss_beer_weighted_avg_score <- nrow(d.raw[which(is.na(d.raw$beer_weighted_avg_score)), ])
+  cat(paste0("Missing beer weighted avg score: ", miss_beer_weighted_avg_score), "\n")
+  
+  miss_beer_num_ratings <- nrow(d.raw[which(is.na(d.raw$beer_num_ratings)), ])
+  cat(paste0("Missing beer num ratings: ", miss_beer_num_ratings), "\n")
+  
+  miss_beer_global_score <- nrow(d.raw[which(is.na(d.raw$beer_global_score)), ])
+  cat(paste0("Missing beer global score: ", miss_beer_global_score), "\n")
+  
+  miss_beer_global_style_score <- nrow(d.raw[which(is.na(d.raw$beer_global_style_score)), ])
+  cat(paste0("Missing beer global style score: ", miss_beer_global_style_score), "\n")
+  
+  miss_beer_ABV <- nrow(d.raw[which(is.na(d.raw$beer_ABV)), ])
+  cat(paste0("Missing beer ABV: ", miss_beer_ABV), "\n")
+  
+  miss_beer_style <- nrow(d.raw[which(is.na(d.raw$beer_style)), ])
+  cat(paste0("Missing beer style: ", miss_beer_style), "\n")
+  
+  miss_beer_location <- nrow(d.raw[which(is.na(d.raw$beer_location)), ])
+  cat(paste0("Missing beer location: ", miss_beer_location), "\n")
+  
+  miss_beer_num_calories <- nrow(d.raw[which(is.na(d.raw$beer_num_calories)), ])
+  cat(paste0("Missing beer num calories: ", miss_beer_num_calories), "\n")
+  
+  miss_beer_brewer_name <- nrow(d.raw[which(is.na(d.raw$beer_brewer_name)), ])
+  cat(paste0("Missing beer brewer name: ", miss_beer_brewer_name), "\n")
+  
+  cat(paste0("-------------- USER ----------------"), "\n")
+  cat(paste0("===================================="), "\n")
+  
+  miss_user_num_ratings <- nrow(d.raw[which(is.na(d.raw$user_num_ratings)), ])
+  cat(paste0("Missing user num ratings: ", miss_user_num_ratings), "\n")
+  
+  miss_user_num_places_rated <- nrow(d.raw[which(is.na(d.raw$user_num_places_rated)), ])
+  cat(paste0("Missing user num places rated: ", miss_user_num_places_rated), "\n")
+  
+  miss_user_location <- nrow(d.raw[which(is.na(d.raw$user_location)), ])
+  cat(paste0("Missing user location: ", miss_user_location), "\n")
+  
+  miss_user_num_friends <- nrow(d.raw[which(is.na(d.raw$user_num_friends)), ])
+  cat(paste0("Missing user num countries rated: ", miss_user_num_friends), "\n")
+  
+  miss_user_num_following <- nrow(d.raw[which(is.na(d.raw$user_num_following)), ])
+  cat(paste0("Missing user num following: ", miss_user_num_following), "\n")
+  
+  miss_user_name <- nrow(d.raw[which(is.na(d.raw$user_name)), ])
+  cat(paste0("Missing user name: ", miss_user_name), "\n")
+  
+  miss_user_num_breweries_rated <- nrow(d.raw[which(is.na(d.raw$user_num_breweries_rated)), ])
+  cat(paste0("Missing user num breweries rated: ", miss_user_num_breweries_rated), "\n")
+  
+  miss_user_num_countries_rated <- nrow(d.raw[which(is.na(d.raw$user_num_countries_rated)), ])
+  cat(paste0("Missing user num countries rated: ", miss_user_num_countries_rated), "\n")
+}
+missing_data_summary(d.raw)
+
+
+
+### Quick work to take care of missing overall scores (20pt scores)
+urls <- d.raw[which(is.na(d.raw$review_overall_score)), ]$user_url
+pattern <- '([0-9]{3,6})'
+missing_overall_score_ids <- unique(sapply(urls, function(url) {
+  str_match(url, pattern)[, 2]  
+}))
+
+# write.csv(missing_overall_score_ids, file = "missingOverallScores.csv")
+
+
