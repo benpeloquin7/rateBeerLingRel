79979169767233c8553959300bbb8296cbef8ba4
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-05-17 21:59:13 -0700

    added trigram model and fixed bigram / unigrams...

--- analysis/LanguageModel.py
+++ analysis/LanguageModel.py
@@ -16,6 +16,7 @@ class LanguageModel:
 	def __init__(self, verbose=False, full_vocab=None, ngram_range=(1, 3)):
 		self.full_vocab = full_vocab            ## Do we want to store all possible words?
 		self.training_vocab = set()
+		self.training_total_tokens = 0
 		self.ngrams_dict = defaultdict(lambda:0) ## Holds all unigrams, bigrams and trigrams
 
 
@@ -42,8 +43,12 @@ class LanguageModel:
 		self.ngram_vectorizer.fit_transform(self.raw_data)                               ## Vocab
 		self.ngram_count_matrix = self.ngram_vectorizer.transform(self.raw_data)         ## Counts
 		for word, count in zip(self.ngram_vectorizer.get_feature_names(), np.asarray(self.ngram_count_matrix.sum(axis=0)).ravel()):
-			## Save all unigrams to vocavular
-			if len(word.split()) == 1: self.training_vocab.add(word)
+			
+			## Save all unigrams to vocab and get total token counts
+			if len(word.split()) == 1:
+				self.training_vocab.add(word)
+				self.training_total_tokens += count
+			## Populate to ngrams dict
 			self.ngrams_dict[word] = count
 
 		self.training_vocab_size = len(self.training_vocab) + 1 ## Add one for UNK
@@ -52,44 +57,35 @@ class LanguageModel:
 		return gram.split()[0]
 
 	def get_leading_bigram(self, gram):
-		return (' ').join(gram.split()[:1])
-
+		return (' ').join(gram.split()[:2])
 
 	def get_words_and_counts(self):
 		return([(word, count) for word, count in self.ngrams_dict.items()])
 
 	def score(self, curr_example):
 		return 0
-		
-		# example_length = len(curr_example.split())
-
-		# ## In the case of the bigram model avoid one word case
-		# if example_length < 2:
-		# 	return 0
-
-		# score = 0
-
-		# ## unigrams
-		# # unigram_vectorizer = CountVectorizer(ngram_range=(1,1))
-		# # unigram_vectorizer.fit_transform([curr_example])
-		# # unigram_count_matrix = unigram_vectorizer.transform(curr_example)
-		# ## bigrams
-		# bigram_vectorizer = CountVectorizer(ngram_range=(2,2)) ## Keep the 1 so that we can handle one word reviews
-		# bigram_vectorizer.fit_transform([curr_example])
-		# bigram_count_matrix = bigram_vectorizer.transform([curr_example])
 
 
-		# ## bigram score calculation
-		# for gram, count in zip(bigram_vectorizer.get_feature_names(), np.asarray(bigram_count_matrix.sum(axis=0)).ravel()):
-		# 	score += count * math.log(self.ngrams_dict[gram] + 1)            ## Laplace smoothing with +1 / UNK implicitly
-		# 	leading_unigram = self.get_leading_unigram(gram)
-		# 	score -= count * math.log(self.ngrams_dict[leading_unigram] + self.training_vocab_size) ## Laplace smoothing with +1 / UNK with max(x, 1)
+class UnigramLM_Laplace(LanguageModel):
 
-		# exponent = -float(1) / example_length
-		# pp_score = math.pow(math.exp(score), exponent)
+	def score(self, curr_example):		
+		example_length = len(curr_example.split())
 
-		# return pp_score
+		score = 0
 
+		unigram_vectorizer = CountVectorizer(ngram_range=(1,1))
+		unigram_vectorizer.fit_transform([curr_example])
+		unigram_count_matrix = unigram_vectorizer.transform([curr_example])
+
+		## bigram score calculation
+		for gram, count in zip(unigram_vectorizer.get_feature_names(), np.asarray(unigram_count_matrix.sum(axis=0)).ravel()):
+			score += math.log(count) + math.log(self.ngrams_dict[gram] + 1)            ## Laplace smoothing with +1 / UNK implicitly
+			score -= math.log(count) + math.log(self.training_vocab_size + self.training_total_tokens) ## Laplace smoothing with +1 / UNK with max(x, 1)
+
+		exponent = -float(1) / example_length
+		pp_score = math.pow(math.exp(score), exponent)
+
+		return pp_score
 
 class BigramLM_Laplace(LanguageModel):
 
@@ -98,25 +94,54 @@ class BigramLM_Laplace(LanguageModel):
 
 		## In the case of the bigram model avoid one word case
 		if example_length < 2:
-			return 0
+			return float('inf')
 
 		score = 0
 
-		bigram_vectorizer = CountVectorizer(ngram_range=(2,2)) ## Keep the 1 so that we can handle one word reviews
+		bigram_vectorizer = CountVectorizer(ngram_range=(2,2))
 		bigram_vectorizer.fit_transform([curr_example])
 		bigram_count_matrix = bigram_vectorizer.transform([curr_example])
 
 		## bigram score calculation
 		for gram, count in zip(bigram_vectorizer.get_feature_names(), np.asarray(bigram_count_matrix.sum(axis=0)).ravel()):
-			score += count * math.log(self.ngrams_dict[gram] + 1)            ## Laplace smoothing with +1 / UNK implicitly
+			## Laplace smoothing with +1 / UNK implicitly
+			score += math.log(count) + math.log(self.ngrams_dict[gram] + 1)
 			leading_unigram = self.get_leading_unigram(gram)
-			score -= count * math.log(self.ngrams_dict[leading_unigram] + self.training_vocab_size) ## Laplace smoothing with +1 / UNK with max(x, 1)
+			## Laplace smoothing with +1 / UNK with max(x, 1)
+			score -= math.log(count) * math.log(self.ngrams_dict[leading_unigram] + self.training_vocab_size)
 
 		exponent = -float(1) / example_length
 		pp_score = math.pow(math.exp(score), exponent)
 
 		return pp_score
 
+class TrigramLM_Laplace(LanguageModel):
+
+	def score(self, curr_example):		
+		example_length = len(curr_example.split())
+
+		## In the case of the trigram model avoid one word case
+		if example_length < 3:
+			return float('inf')
+
+		score = 0
+
+		trigram_vectorizer = CountVectorizer(ngram_range=(3,3))
+		trigram_vectorizer.fit_transform([curr_example])
+		trigram_count_matrix = trigram_vectorizer.transform([curr_example])
+
+		## bigram score calculation
+		for gram, count in zip(trigram_vectorizer.get_feature_names(), np.asarray(trigram_count_matrix.sum(axis=0)).ravel()):
+			## Laplace smoothing with +1 / UNK implicitly
+			score += math.log(count) + math.log(self.ngrams_dict[gram] + 1)
+			leading_bigram = self.get_leading_bigram(gram)
+			## Laplace smoothing with +1 / UNK with max(x, 1)
+			score -= math.log(count) * math.log(self.ngrams_dict[leading_bigram] + self.training_vocab_size)
+
+		exponent = -float(1) / example_length
+		pp_score = math.pow(math.exp(score), exponent)
+
+		return pp_score
 
 if __name__ == '__main__':
 	trial_data = ['hello world', 'hello world oh, hello jim again', 'i am sam']
